d97a2df79da7271f15cbbf26f88c355bd5385e3b
alexins <alexins@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-04-19 10:52:39 +0000

    Заливка кода BE. Этап 7. Продолжение следует...
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@9 d191f3aa-8897-45be-b9a4-56e5fb472b67

---
+++ src/apps/mplayerc/mpciconlib/mpciconlib.h
@@ -0,0 +1,77 @@
+/*
+ * $Id: mpciconlib.h 4345 2012-04-12 08:04:38Z XhmikosR $
+ *
+ * (C) 2008-2012 see Authors.txt
+ *
+ * This file is part of MPC-HC.
+ *
+ * MPC-HC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPC-HC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* The values below correspond to the icon indexes.
+ * The order should be exactly the same as in the mpciconlib.rc file.
+ * There should be no gaps in the numbering. So be careful
+ * when adding/removing icons.
+ */
+
+#define IDI_AVI_ICON                    0
+#define IDI_MPG_ICON                    1
+#define IDI_MP4_ICON                    2
+#define IDI_MKV_ICON                    3
+#define IDI_OGM_ICON                    4
+#define IDI_FLV_ICON                    5
+#define IDI_WMV_ICON                    6
+#define IDI_MOV_ICON                    7
+#define IDI_RM_ICON                     8
+#define IDI_MP3_ICON                    9
+#define IDI_OGG_ICON                    10
+#define IDI_AAC_ICON                    11
+#define IDI_MKA_ICON                    12
+#define IDI_WMA_ICON                    13
+#define IDI_RA_ICON                     14
+#define IDI_WAV_ICON                    15
+#define IDI_MPA_ICON                    16
+#define IDI_CDA_ICON                    17
+#define IDI_MPC_ICON                    18
+#define IDI_FLAC_ICON                   19
+#define IDI_ALAC_ICON                   20
+#define IDI_PLAYLIST_ICON               21
+#define IDI_D2V_ICON                    22
+#define IDI_MID_ICON                    23
+#define IDI_AIFF_ICON                   24
+#define IDI_AU_ICON                     25
+#define IDI_IVF_ICON                    26
+#define IDI_FLIC_ICON                   27
+#define IDI_BIK_ICON                    28
+#define IDI_RATDVD_ICON                 29
+#define IDI_RT_ICON                     30
+#define IDI_DSM_ICON                    31
+#define IDI_SWF_ICON                    32
+#define IDI_OTHER_ICON                  33
+#define IDI_DVDA_ICON                   34
+#define IDI_DVDF_ICON                   35
+#define IDI_NONE                        -1
+
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        39
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
