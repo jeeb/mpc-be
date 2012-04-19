b40ba3abb60816d82a09427cc27db1a860cfb69b
alexins <alexins@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-04-19 13:11:15 +0000

    Изменил номер версии, 1.6.2 -> 0.1.0
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@12 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- include/Version.h
+++ include/Version.h
@@ -46,9 +46,9 @@
 #define DO_MAKE_STR(x) #x
 #define MAKE_STR(x)    DO_MAKE_STR(x)
 
-#define MPC_VERSION_MAJOR 1
-#define MPC_VERSION_MINOR 6
-#define MPC_VERSION_PATCH 2
+#define MPC_VERSION_MAJOR 0
+#define MPC_VERSION_MINOR 1
+#define MPC_VERSION_PATCH 0
 
 
 #ifndef ISPP_IS_BUGGY
