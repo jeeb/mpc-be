2dd2c0b5c7ca2f42ab680170f251885068ae1601
Aleksoid <Aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-05-30 06:08:51 +0000

    Изменение : удалил ненужный идентификатор;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@429 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/MainFrm.cpp
+++ src/apps/mplayerc/MainFrm.cpp
@@ -3176,7 +3176,7 @@ void CMainFrame::OnInitMenuPopup(CMenu* pPopupMenu, UINT nIndex, BOOL bSysMenu)
 		}
 		CMenu* pSubMenu = NULL;
 
-		if (itemID == ID_FILE_OPENDISC32774) {
+		if (itemID == ID_FILE_OPENDISC) {
 			SetupOpenCDSubMenu();
 			pSubMenu = &m_opencds;
 		} else if (itemID == ID_FILTERS) {
