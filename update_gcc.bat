aba02f67fcafb3b38256ce95ea6b9006fce5a790
exodus8 <exodus8@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-07-11 21:14:29 +0000

    Небольшие изменения;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@667 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- update_gcc.bat
+++ update_gcc.bat
@@ -11,6 +11,7 @@ COPY /V /Y "%MINGW32%\i686-pc-mingw32\lib\libpthreadGC2.a" "lib\"
 COPY /V /Y "%MINGW32%\i686-pc-mingw32\lib\libmingwex.a" "lib\"
 COPY /V /Y "%MINGW32%\lib\gcc\i686-pc-mingw32\%gccver%\libgcc.a" "lib\"
 
+COPY /V /Y "%MINGW32%\x86_64-w64-mingw32\lib\libmingwex.a" "lib64\"
 COPY /V /Y "%MINGW32%\lib\gcc\x86_64-w64-mingw32\%gccver%\libgcc.a" "lib64\"
 
 PAUSE
