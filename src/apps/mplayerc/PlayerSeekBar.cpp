f40553015acf0eb8416f675120d6c4ae90b18c08
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-07-26 09:11:16 +0000

    Изменение : Починил вывод окна превью на мультимониторных конфигурациях;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@749 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/PlayerSeekBar.cpp
+++ src/apps/mplayerc/PlayerSeekBar.cpp
@@ -765,7 +765,7 @@ void CPlayerSeekBar::UpdateToolTipPosition(CPoint& point)
 		point.x -= r_width / 2 - 2;
 		point.y = GetChannelRect().TopLeft().y - (r_height + 13);
 		ClientToScreen(&point);
-		point.x = max(5, min(point.x, (mi.rcWork.right - mi.rcWork.left) - r_width - 5));
+		point.x = max(mi.rcWork.left + 5, min(point.x, mi.rcWork.right - r_width - 5));
 		if (point.y <= 5) {
 			p1.y = GetChannelRect().TopLeft().y + 30;
 			ClientToScreen(&p1);
