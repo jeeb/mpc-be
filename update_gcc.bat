0382ed88e2776b3b1fd50298e05230aac0bce20b
alexins <alexins@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-08-01 00:41:02 +0000

    Изменение: обновил файл update_gcc.bat, для использования i686-w64-mingw32 вместо i686-pc-mingw32.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@784 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- update_gcc.bat
+++ update_gcc.bat
@@ -7,9 +7,8 @@ FOR /f "tokens=1,2 delims=" %%K IN (
   SET "gccver=%%K" & Call :SubGCCVer %%gccver:*Z=%%
 )
 
-COPY /V /Y "%MINGW32%\i686-pc-mingw32\lib\libpthreadGC2.a" "lib\"
-COPY /V /Y "%MINGW32%\i686-pc-mingw32\lib\libmingwex.a" "lib\"
-COPY /V /Y "%MINGW32%\lib\gcc\i686-pc-mingw32\%gccver%\libgcc.a" "lib\"
+COPY /V /Y "%MINGW32%\i686-w64-mingw32\lib\libmingwex.a" "lib\"
+COPY /V /Y "%MINGW32%\lib\gcc\i686-w64-mingw32\%gccver%\libgcc.a" "lib\"
 
 COPY /V /Y "%MINGW32%\x86_64-w64-mingw32\lib\libmingwex.a" "lib64\"
 COPY /V /Y "%MINGW32%\lib\gcc\x86_64-w64-mingw32\%gccver%\libgcc.a" "lib64\"
