9c34bc63d967902ba1406879f284f13de4d4a7c1
exodus8 <exodus8@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-08-08 22:48:27 +0000

    Небольшое обновление;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@858 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- docs/Readme.txt
+++ docs/Readme.txt
@@ -47,32 +47,25 @@ MPC-BE makes use of the following 3rd party code:
 Project             License                    Website
 -------------------------------------------------------------------------------------
 ATL Server          Ms-LPL                     http://atlserver.codeplex.com/
-Bento4              GPLv2                      http://zebulon.bok.net/trac/Bento4
+Bento4              GPLv2                      http://zebulon.bok.net/trac/Bento4/
 CFileVersionInfo    -                          -
 CLineNumberEdit     -                          -
-CSizingControlBar   GPLv2                      http://www.datamekanix.com/sizecbar/
+CSizingControlBar   GPLv2                      http://datamekanix.com/sizecbar/
 Detours             MSR-SSLA                   http://research.microsoft.com/en-us/projects/detours/
-ffmpeg              GPLv2                      http://www.ffmpeg.org/
-liba52              GPLv2                      http://liba52.sourceforge.net/
-libav               GPLv2                      http://libav.org/
-libdca              GPLv2                      http://www.videolan.org/developers/libdca.html
-libflac             GPLv2 / New BSD License    http://flac.sourceforge.net/
-libogg              New BSD License            http://www.xiph.org/
-libpng              libpng License             http://libpng.sourceforge.net/
+FFmpeg              GPLv3                      http://ffmpeg.org/
+libflac             GPLv2 / New BSD License    http://sourceforge.net/projects/flac/
+libpng              libpng License             http://sourceforge.net/projects/libpng/
 libwebp             BSD License                http://code.google.com/p/webp/
-Little CMS          MIT License                http://www.littlecms.com/
+Little CMS          MIT License                http://littlecms.com/
 Logitech SDK        -                          -
-MediaInfoLib        LGPLv3                     http://mediainfo.sourceforge.net/
-MultiMon            CPOL                       http://www.codeproject.com/KB/GDI/multimon.aspx
-mpeg2dec            GPLv2                      http://libmpeg2.sourceforge.net/
-pngdib              -                          http://entropymine.com/jason/pngdib/
+MediaInfoLib        LGPLv3                     http://sourceforge.net/projects/mediainfo/
+MusePack            GPL                        http://musepack.net/
 QuickTime SDK       -                          -
 RealMedia SDK       -                          -
 ResizableLib        Artistic License           http://sourceforge.net/projects/resizablelib/
-SoundTouch          LGPLv2.1                   http://www.surina.net/soundtouch/
+SoundTouch          LGPLv2.1                   http://surina.net/soundtouch/
 TreePropSheet       -                          -
-VirtualDub          GPLv2                      http://www.virtualdub.org/
+VirtualDub          GPLv2                      http://virtualdub.org/
+WavPackLib          BSD License                http://wavpack.com/
 ZenLib              Simplified BSD License     http://sourceforge.net/projects/zenlib/
 zlib                zlib License               http://zlib.net/
-WavPack             BSD License                http://wavpack.com/
-MusePack            GPL                        http://musepack.net/
--- src/DSUtil/H264Nalu.cpp
+++ src/DSUtil/H264Nalu.cpp
@@ -1,49 +1,49 @@
 /*
- * $Id$
+ *      Copyright (C) 2010-2012 Hendrik Leppkes
+ *      http://www.1f0.de
  *
- * (C) 2006-2012 see Authors.txt
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * This file is part of MPC-BE.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * MPC-BE is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * MPC-BE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
 #include "stdafx.h"
 #include "H264Nalu.h"
 
-void CH264Nalu::SetBuffer(BYTE* pBuffer, int nSize, int nNALSize)
+void CH264Nalu::SetBuffer(const BYTE* pBuffer, size_t nSize, int nNALSize)
 {
-	m_pBuffer		= pBuffer;
-	m_nSize			= nSize;
-	m_nNALSize		= nNALSize;
-	m_nCurPos		= 0;
-	m_nNextRTP		= 0;
+  m_pBuffer      = pBuffer;
+  m_nSize        = nSize;
+  m_nNALSize     = nNALSize;
+  m_nCurPos      = 0;
+  m_nNextRTP     = 0;
 
-	m_nNALStartPos	= 0;
-	m_nNALDataPos	= 0;
+  m_nNALStartPos = 0;
+  m_nNALDataPos  = 0;
 
-	if (!nNALSize && nSize) {
-		MoveToNextAnnexBStartcode();
-	}
+  if (nNALSize == 0 && nSize > 0)
+    MoveToNextAnnexBStartcode();
 }
 
 bool CH264Nalu::MoveToNextAnnexBStartcode()
 {
-	int nBuffEnd = m_nSize - 4;
+	if (m_nSize < 4)
+		return false;
+	size_t nBuffEnd = m_nSize - 4;
 
-	for (int i=m_nCurPos; i<nBuffEnd; i++) {
+	for (size_t i=m_nCurPos; i<nBuffEnd; i++) {
 		if ((*((DWORD*)(m_pBuffer+i)) & 0x00FFFFFF) == 0x00010000) {
 			// Find next AnnexB Nal
 			m_nCurPos = i;
@@ -68,36 +68,39 @@ bool CH264Nalu::MoveToNextRTPStartcode()
 
 bool CH264Nalu::ReadNext()
 {
-	if ((m_nCurPos >= m_nSize) || (m_nCurPos<0)) {
-		return false;
-	}
 
-	if ((m_nNALSize != 0) && (m_nCurPos == m_nNextRTP)) {
-		// RTP Nalu type : (XX XX) XX XX NAL..., with XX XX XX XX or XX XX equal to NAL size
-		m_nNALStartPos	= m_nCurPos;
-		m_nNALDataPos	= m_nCurPos + m_nNALSize;
-		unsigned nTemp	= 0;
-		for (int i=0; i<m_nNALSize; i++) {
-			nTemp = (nTemp << 8) + m_pBuffer[m_nCurPos++];
-		}
-		m_nNextRTP += nTemp + m_nNALSize;
-		MoveToNextRTPStartcode();
-	} else {
-		// Remove trailing bits
-		while (m_pBuffer[m_nCurPos]==0x00 && ((*((DWORD*)(m_pBuffer+m_nCurPos)) & 0x00FFFFFF) != 0x00010000)) {
-			m_nCurPos++;
-		}
+  if (m_nCurPos >= m_nSize) return false;
 
-		// AnnexB Nalu : 00 00 01 NAL...
-		m_nNALStartPos	= m_nCurPos;
-		m_nCurPos	   += 3;
-		m_nNALDataPos	= m_nCurPos;
-		MoveToNextAnnexBStartcode();
-	}
+  if ((m_nNALSize != 0) && (m_nCurPos == m_nNextRTP))
+  {
+    if (m_nCurPos+m_nNALSize >= m_nSize) return false;
+    // RTP Nalu type : (XX XX) XX XX NAL..., with XX XX XX XX or XX XX equal to NAL size
+    m_nNALStartPos = m_nCurPos;
+    m_nNALDataPos  = m_nCurPos + m_nNALSize;
+    unsigned nTemp = 0;
+    for (int i=0; i<m_nNALSize; i++)
+    {
+      nTemp = (nTemp << 8) + m_pBuffer[m_nCurPos++];
+    }
+    m_nNextRTP += nTemp + m_nNALSize;
+    MoveToNextRTPStartcode();
+  }
+  else
+  {
+    // Remove trailing bits
+    while (m_pBuffer[m_nCurPos]==0x00 && ((*((DWORD*)(m_pBuffer+m_nCurPos)) & 0x00FFFFFF) != 0x00010000))
+      m_nCurPos++;
 
-	forbidden_bit		= (m_pBuffer[m_nNALDataPos]>>7) & 1;
-	nal_reference_idc	= (m_pBuffer[m_nNALDataPos]>>5) & 3;
-	nal_unit_type		= (NALU_TYPE) (m_pBuffer[m_nNALDataPos] & 0x1f);
+    // AnnexB Nalu : 00 00 01 NAL...
+    m_nNALStartPos = m_nCurPos;
+    m_nCurPos     += 3;
+    m_nNALDataPos  = m_nCurPos;
+    MoveToNextAnnexBStartcode();
+  }
 
-	return true;
+  forbidden_bit     = (m_pBuffer[m_nNALDataPos]>>7) & 1;
+  nal_reference_idc = (m_pBuffer[m_nNALDataPos]>>5) & 3;
+  nal_unit_type     = (NALU_TYPE) (m_pBuffer[m_nNALDataPos] & 0x1f);
+
+  return true;
 }
