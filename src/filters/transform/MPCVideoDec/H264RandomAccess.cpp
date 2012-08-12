93ba3ea8d38a44bdeb1c9925959483ed80ddc629
exodus8 <exodus8@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-08-12 16:39:25 +0000

    Обновил копирайты;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@876 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/transform/MPCVideoDec/H264RandomAccess.cpp
+++ src/filters/transform/MPCVideoDec/H264RandomAccess.cpp
@@ -1,20 +1,24 @@
 /*
- *      Copyright (C) 2010-2012 Hendrik Leppkes
- *      http://www.1f0.de
+ * $Id$
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * (C) 2003-2006 Gabest
+ * (C) 2006-2012 see Authors.txt
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This file is part of MPC-BE.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * MPC-BE is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPC-BE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include "stdafx.h"
@@ -24,7 +28,7 @@
 
 CH264RandomAccess::CH264RandomAccess()
 {
-  flush(0);
+	flush(0);
 }
 
 CH264RandomAccess::~CH264RandomAccess()
@@ -33,59 +37,59 @@ CH264RandomAccess::~CH264RandomAccess()
 
 void CH264RandomAccess::flush(int threadCount)
 {
-  m_RecoveryMode = 1;
-  m_RecoveryFrameCount = 0;
-  m_ThreadDelay = threadCount;
+	m_RecoveryMode = 1;
+	m_RecoveryFrameCount = 0;
+	m_ThreadDelay = threadCount;
 }
 
 BOOL CH264RandomAccess::searchRecoveryPoint(AVCodecContext* pAVCtx, BYTE *buf, int buf_size)
 {
-  if (m_RecoveryMode == 1) {
-    int recoveryPoint = avcodec_h264_search_recovery_point(pAVCtx, buf, buf_size, &m_RecoveryFrameCount);
-    switch (recoveryPoint) {
-    case 3: // IDRS
-      m_RecoveryMode = 0;
-      return TRUE;
-    case 2: // SEI recovery
-      m_RecoveryMode = 2;
-      return TRUE;
-    case 1: // I Frame
-      m_RecoveryMode = 2;
-      m_RecoveryFrameCount = 0;
-      return TRUE;
-    default:
-      return FALSE;
-    }
-  } else {
-    return TRUE;
-  }
+	if (m_RecoveryMode == 1) {
+		int recoveryPoint = avcodec_h264_search_recovery_point(pAVCtx, buf, buf_size, &m_RecoveryFrameCount);
+		switch (recoveryPoint) {
+			case 3: // IDRS
+				m_RecoveryMode = 0;
+				return TRUE;
+			case 2: // SEI recovery
+				m_RecoveryMode = 2;
+				return TRUE;
+			case 1: // I Frame
+				m_RecoveryMode = 2;
+				m_RecoveryFrameCount = 0;
+				return TRUE;
+			default:
+				return FALSE;
+		}
+	} else {
+		return TRUE;
+	}
 }
 
 void CH264RandomAccess::judgeFrameUsability(AVFrame *pFrame, int *got_picture_ptr)
 {
-  if ((m_ThreadDelay > 1 && --m_ThreadDelay > 0) || m_RecoveryMode == 0 || pFrame->h264_max_frame_num == 0) {
-    return;
-  }
+	if ((m_ThreadDelay > 1 && --m_ThreadDelay > 0) || m_RecoveryMode == 0 || pFrame->h264_max_frame_num == 0) {
+		return;
+	}
 
-  if (m_RecoveryMode == 1 || m_RecoveryMode == 2) {
-    m_RecoveryFrameCount = (m_RecoveryFrameCount + pFrame->h264_frame_num_decoded) % pFrame->h264_max_frame_num;
-    m_RecoveryMode = 3;
-  }
+	if (m_RecoveryMode == 1 || m_RecoveryMode == 2) {
+		m_RecoveryFrameCount = (m_RecoveryFrameCount + pFrame->h264_frame_num_decoded) % pFrame->h264_max_frame_num;
+		m_RecoveryMode = 3;
+	}
 
-  if (m_RecoveryMode == 3) {
-    if (m_RecoveryFrameCount <= pFrame->h264_frame_num_decoded) {
-      m_RecoveryPOC = pFrame->h264_poc_decoded;
-      m_RecoveryMode = 4;
-    }
-  }
+	if (m_RecoveryMode == 3) {
+		if (m_RecoveryFrameCount <= pFrame->h264_frame_num_decoded) {
+			m_RecoveryPOC = pFrame->h264_poc_decoded;
+			m_RecoveryMode = 4;
+		}
+	}
 
-  if (m_RecoveryMode == 4) {
-    if (pFrame->h264_poc_outputed >= m_RecoveryPOC) {
-      m_RecoveryMode = 0;
-    }
-  }
+	if (m_RecoveryMode == 4) {
+		if (pFrame->h264_poc_outputed >= m_RecoveryPOC) {
+			m_RecoveryMode = 0;
+		}
+	}
 
-  if (m_RecoveryMode != 0) {
-    *got_picture_ptr = 0;
-  }
+	if (m_RecoveryMode != 0) {
+		*got_picture_ptr = 0;
+	}
 }
--- src/filters/transform/MPCVideoDec/H264RandomAccess.h
+++ src/filters/transform/MPCVideoDec/H264RandomAccess.h
@@ -1,20 +1,24 @@
 /*
- *      Copyright (C) 2010-2012 Hendrik Leppkes
- *      http://www.1f0.de
+ * $Id$
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * (C) 2003-2006 Gabest
+ * (C) 2006-2012 see Authors.txt
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This file is part of MPC-BE.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * MPC-BE is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPC-BE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #pragma once
@@ -25,16 +29,16 @@ struct AVFrame;
 class CH264RandomAccess
 {
 public:
-  CH264RandomAccess();
-  ~CH264RandomAccess();
+	CH264RandomAccess();
+	~CH264RandomAccess();
 
-  void flush(int threadCount);
-  BOOL searchRecoveryPoint(AVCodecContext* pAVCtx, BYTE *buf, int buf_size);
-  void judgeFrameUsability(AVFrame *pFrame, int *got_picture_ptr);
+	void flush(int threadCount);
+	BOOL searchRecoveryPoint(AVCodecContext* pAVCtx, BYTE *buf, int buf_size);
+	void judgeFrameUsability(AVFrame *pFrame, int *got_picture_ptr);
 
 private:
-  int m_RecoveryMode; // 0: OK; 1: Searching; 2: Found; 3: 
-  int m_RecoveryFrameCount;
-  int m_RecoveryPOC;
-  int m_ThreadDelay;
+	int m_RecoveryMode; // 0: OK; 1: Searching; 2: Found; 3: 
+	int m_RecoveryFrameCount;
+	int m_RecoveryPOC;
+	int m_ThreadDelay;
 };
