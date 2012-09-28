4655ba94fdfe9451d0cb7deb04c264a7dbf96df7
szl <szl@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-09-28 05:00:07 +0000

    Обновление: ffmpeg git-4fefe91;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@1249 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/ExtLib/ffmpeg/libavcodec/mpegaudiodec.c
+++ src/ExtLib/ffmpeg/libavcodec/mpegaudiodec.c
@@ -205,6 +205,8 @@ static void ff_compute_band_indexes(MPADecodeContext *s, GranuleDef *g)
 {
     if (g->block_type == 2) {
         if (g->switch_point) {
+            if(s->sample_rate_index == 8)
+                av_log_ask_for_sample(s->avctx, "switch point in 8khz\n");
             /* if switched mode, we handle the 36 first samples as
                 long blocks.  For 8000Hz, we handle the 72 first
                 exponents as long blocks */
@@ -213,7 +215,7 @@ static void ff_compute_band_indexes(MPADecodeContext *s, GranuleDef *g)
             else
                 g->long_end = 6;
 
-            g->short_start = 2 + (s->sample_rate_index != 8);
+            g->short_start = 3;
         } else {
             g->long_end    = 0;
             g->short_start = 0;
