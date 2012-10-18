8f0b44ae2a55f0581792401ee87df5e8e32437a4
exodus8 <exodus8@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-10-18 21:12:46 +0000

    Изменение: подправил позиционирование FlyBar;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@1403 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/MainFrm.cpp
+++ src/apps/mplayerc/MainFrm.cpp
@@ -1433,14 +1433,13 @@ bool CMainFrame::FlyBarSetPos()
 		BITMAP bm;
 		::GetObject(hBmp, sizeof(bm), &bm);
 		DeleteObject(hBmp);
-		int pos = (bm.bmHeight * 9) + (bm.bmHeight / 8) + 6;
+		int pos = (bm.bmHeight * 9) + 8;
 
 		m_wndFlyBar.MoveWindow(r_wndView.right-4-pos, r_wndView.top+4, pos, bm.bmHeight+8);
 		m_wndFlyBar.CalcButtonsRect();
 		if (r_wndView.bottom-r_wndView.top > 40 && r_wndView.right-r_wndView.left > 236) {
 			if (AfxGetAppSettings().fFlybarOnTop && !m_wndFlyBar.IsWindowVisible()) {
 				m_wndFlyBar.ShowWindow(SW_SHOWNOACTIVATE);
-				
 			}
 			return 1;
 		} else {
