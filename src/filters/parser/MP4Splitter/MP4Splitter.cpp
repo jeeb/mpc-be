cafa74ba6ef423038e62a663315e06ee084eabdb
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2012-12-07 03:51:52 +0000

    Изменение : MP4Splitter - исправил определение E-AC3 дорожек и формирование выходного MediaType;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@1670 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/parser/MP4Splitter/MP4Splitter.cpp
+++ src/filters/parser/MP4Splitter/MP4Splitter.cpp
@@ -1050,11 +1050,12 @@ HRESULT CMP4SplitterFilter::CreateOutputs(IAsyncReader* pAsyncReader)
 							SetTrackName(&TrackName, _T("MPEG Audio (MP3)"));
 							fourcc = WAVE_FORMAT_MPEGLAYER3;
 						} else if ((type == AP4_ATOM_TYPE__AC3) || (type == AP4_ATOM_TYPE_SAC3) || (type == AP4_ATOM_TYPE_EAC3)) {
-							fourcc = 0x2000;
 							if (type == AP4_ATOM_TYPE_EAC3) {
-								SetTrackName(&TrackName, _T("AC-3 Audio"));
-							} else {
 								SetTrackName(&TrackName, _T("Enhanced AC-3 audio"));
+								fourcc = WAVE_FORMAT_UNKNOWN;
+							} else {
+								fourcc = WAVE_FORMAT_DOLBY_AC3;
+								SetTrackName(&TrackName, _T("AC-3 Audio"));
 							}
 						} else if (type == AP4_ATOM_TYPE_MP4A) {
 							fourcc = WAVE_FORMAT_AAC;
@@ -1166,7 +1167,9 @@ HRESULT CMP4SplitterFilter::CreateOutputs(IAsyncReader* pAsyncReader)
 						}
 
 						mt.subtype = FOURCCMap(fourcc);
-
+						if (type == AP4_ATOM_TYPE_EAC3) {
+							mt.subtype = MEDIASUBTYPE_DOLBY_DDPLUS;
+						}
 
 						if (type == AP4_ATOM_TYPE('m', 's', 0x00, 0x02)) {
 							const WORD numcoef = 7;
