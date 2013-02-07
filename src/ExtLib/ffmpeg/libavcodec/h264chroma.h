829fd81d383fb14a48f7f24a8d49a37d15911ce3
szl <szl@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-02-07 21:09:22 +0000

    Обновление: ffmpeg git-c0a4871c.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2046 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/ExtLib/ffmpeg/libavcodec/h264chroma.h
+++ src/ExtLib/ffmpeg/libavcodec/h264chroma.h
@@ -1,13 +1,4 @@
 /*
- * SVQ1 decoder
- * ported to MPlayer by Arpi <arpi@thot.banki.hu>
- * ported to libavcodec by Nick Kurshev <nickols_k@mail.ru>
- *
- * Copyright (C) 2002 the xine project
- * Copyright (C) 2002 the ffmpeg project
- *
- * SVQ1 Encoder (c) 2004 Mike Melanson <melanson@pcisys.net>
- *
  * This file is part of FFmpeg.
  *
  * FFmpeg is free software; you can redistribute it and/or
@@ -25,19 +16,23 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-/**
- * @file
- * Sorenson Vector Quantizer #1 (SVQ1) video codec.
- * For more information of the SVQ1 algorithm, visit:
- *   http://www.pcisys.net/~melanson/codecs/
- */
+#ifndef AVCODEC_H264CHROMA_H
+#define AVCODEC_H264CHROMA_H
 
-#include "svq1.h"
-#include "svq1_cb.h"
-#include "svq1_vlc.h"
+#include <stdint.h>
 
-/* standard video sizes */
-const uint16_t ff_svq1_frame_size_table[7][2] = {
-    { 160, 120 }, { 128,  96 }, { 176, 144 }, { 352, 288 },
-    { 704, 576 }, { 240, 180 }, { 320, 240 }
-};
+typedef void (*h264_chroma_mc_func)(uint8_t *dst/*align 8*/, uint8_t *src/*align 1*/, int srcStride, int h, int x, int y);
+
+typedef struct H264ChromaContext {
+    h264_chroma_mc_func put_h264_chroma_pixels_tab[3];
+    h264_chroma_mc_func avg_h264_chroma_pixels_tab[3];
+} H264ChromaContext;
+
+void ff_h264chroma_init(H264ChromaContext *c, int bit_depth);
+
+void ff_h264chroma_init_arm(H264ChromaContext *c, int bit_depth);
+void ff_h264chroma_init_ppc(H264ChromaContext *c, int bit_depth);
+void ff_h264chroma_init_sh4(H264ChromaContext *c, int bit_depth);
+void ff_h264chroma_init_x86(H264ChromaContext *c, int bit_depth);
+
+#endif /* AVCODEC_H264CHROMA_H */
