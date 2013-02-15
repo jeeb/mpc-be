0f48bb0779b4993a55b38950d2e9cde0c348796b
exodus8 <exodus8@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-02-15 19:29:49 +0000

    Изменение: откат r.2052,2053 так как все равно ни чего не уменьшилось, а только увеличилось :( Обновление: libpng 1.5.15 beta4; Обновление: libwebp 0.2.1 git-92668da6;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2083 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/ExtLib/libwebp/enc/vp8enci.h
+++ src/ExtLib/libwebp/enc/vp8enci.h
@@ -157,7 +157,6 @@ typedef int64_t score_t;     // type used for scores, rate, distortion
 static WEBP_INLINE int QUANTDIV(int n, int iQ, int B) {
   return (n * iQ + B) >> QFIX;
 }
-extern const uint8_t VP8Zigzag[16];
 
 // size of histogram used by CollectHistogram.
 #define MAX_COEFF_THRESH   31
@@ -390,6 +389,7 @@ struct VP8Encoder {
   VP8SegmentInfo dqm_[NUM_MB_SEGMENTS];
   int base_quant_;                 // nominal quantizer value. Only used
                                    // for relative coding of segments' quant.
+  int alpha_;                      // global susceptibility (<=> complexity)
   int uv_alpha_;                   // U/V quantization susceptibility
   // global offset of quantizers, shared by all segments
   int dq_y1_dc_;
