4a126846150cf7646c2fcabb4695856ccf920e43
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-03-07 04:03:25 +0000

    Изменение : небольшая корректировка в механизме обработки кривых PTS;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2220 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/parser/BaseSplitter/BaseSplitter.cpp
+++ src/filters/parser/BaseSplitter/BaseSplitter.cpp
@@ -528,7 +528,7 @@ HRESULT CBaseSplitterOutputPin::DeliverPacket(CAutoPtr<Packet> p)
 		REFERENCE_TIME rt = p->rtStart + m_rtOffset;
 
 		// Filter invalid PTS (if too different from previous packet)
-		if (!IsDiscontinuous() && m_rtPrev != Packet::INVALID_TIME)
+		if (!IsDiscontinuous() && m_rtPrev != Packet::INVALID_TIME && (p->rtStart >= 0))
 			if (_abs64(rt - m_rtPrev) > MAX_PTS_SHIFT) {
 				m_rtOffset += m_rtPrev - rt;
 			}
