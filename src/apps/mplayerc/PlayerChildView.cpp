82b400765d0784fdbeaf24e34f2e4b17f90496e5
judelaw <judelaw@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-03-09 14:57:28 +0000

    Изменение : окно оставалось resizable в режиме borderless и maximized
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2240 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/PlayerChildView.cpp
+++ src/apps/mplayerc/PlayerChildView.cpp
@@ -315,8 +315,11 @@ LRESULT CChildView::OnNcHitTest(CPoint point)
 	UINT nHitTest = CWnd::OnNcHitTest(point);
 
 	CMainFrame* pFrame = ((CMainFrame*)GetParentFrame());
-	//bool fLeftMouseBtnUnassigned = !AssignedToCmd(wmcmd::LDOWN);
-	if (!pFrame->m_fFullScreen && /*(pFrame->IsCaptionHidden() || fLeftMouseBtnUnassigned) && */AfxGetAppSettings().iCaptionMenuMode==MODE_BORDERLESS) {
+
+	WINDOWPLACEMENT wp;
+	pFrame->GetWindowPlacement(&wp);
+
+	if (!pFrame->m_fFullScreen && wp.showCmd != SW_SHOWMAXIMIZED && AfxGetAppSettings().iCaptionMenuMode==MODE_BORDERLESS) {
 		CRect rcClient, rcFrame;
 		GetWindowRect(&rcFrame);
 		rcClient = rcFrame;
