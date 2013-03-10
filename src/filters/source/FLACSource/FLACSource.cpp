fa6e93f7ff54dacdc8075f5767d55f84a0a83e5d
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-03-10 23:05:22 +0000

    Изменение : FLACSource - исправил перемотку на больших файлах, при расчете позиции происходило переполнение для типа uint64.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2261 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/source/FLACSource/FLACSource.cpp
+++ src/filters/source/FLACSource/FLACSource.cpp
@@ -308,7 +308,8 @@ HRESULT CFLACStream::FillBuffer(IMediaSample* pSample, int nFrame, BYTE* pOut, l
 	FLAC__uint64	llNextPos;
 
 	if (m_bDiscontinuity) {
-		FLAC__stream_decoder_seek_absolute (_DECODER_, FLAC__uint64(m_rtPosition * m_i64TotalNumSamples) / m_rtDuration);
+		FLAC__stream_decoder_seek_absolute (_DECODER_, FLAC__uint64((1.0 * m_rtPosition * m_i64TotalNumSamples) / m_rtDuration));
+		// need calculate in double, because uint64 can give overflow
 	}
 
 	FLAC__stream_decoder_get_decode_position(_DECODER_, &llCurPos);
