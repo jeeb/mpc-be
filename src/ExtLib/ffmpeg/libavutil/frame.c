b1c3f7fce74ff037b1acc44f9c211dfb041392c6
szl <szl@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-03-10 18:22:38 +0000

    Обновление: ffmpeg git-35135a39.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2259 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/ExtLib/ffmpeg/libavutil/frame.c
+++ src/ExtLib/ffmpeg/libavutil/frame.c
@@ -49,7 +49,13 @@ static void get_frame_defaults(AVFrame *frame)
 
     memset(frame, 0, sizeof(*frame));
 
-    frame->pts                 = AV_NOPTS_VALUE;
+    frame->pts                   =
+    frame->pkt_dts               =
+    frame->pkt_pts               = AV_NOPTS_VALUE;
+    av_frame_set_best_effort_timestamp(frame, AV_NOPTS_VALUE);
+    av_frame_set_pkt_duration         (frame, 0);
+    av_frame_set_pkt_pos              (frame, -1);
+    av_frame_set_pkt_size             (frame, -1);
     frame->key_frame           = 1;
     frame->sample_aspect_ratio = (AVRational){ 0, 1 };
     frame->format              = -1; /* unknown */
@@ -63,6 +69,7 @@ AVFrame *av_frame_alloc(void)
     if (!frame)
         return NULL;
 
+    frame->extended_data = NULL;
     get_frame_defaults(frame);
 
     return frame;
@@ -194,6 +201,7 @@ int av_frame_ref(AVFrame *dst, AVFrame *src)
     dst->format         = src->format;
     dst->width          = src->width;
     dst->height         = src->height;
+    dst->channels       = src->channels;
     dst->channel_layout = src->channel_layout;
     dst->nb_samples     = src->nb_samples;
 
@@ -389,6 +397,7 @@ int av_frame_copy_props(AVFrame *dst, const AVFrame *src)
     dst->opaque              = src->opaque;
     dst->pkt_pts             = src->pkt_pts;
     dst->pkt_dts             = src->pkt_dts;
+    dst->pkt_pos             = src->pkt_pos;
     dst->quality             = src->quality;
     dst->coded_picture_number = src->coded_picture_number;
     dst->display_picture_number = src->display_picture_number;
