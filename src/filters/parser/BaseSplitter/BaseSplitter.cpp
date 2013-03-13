ba0f137252dc43719c4214d24060e08ac900801b
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-03-13 12:43:49 +0000

    Изменение : переменные надо инициализировать :);
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2273 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/parser/BaseSplitter/BaseSplitter.cpp
+++ src/filters/parser/BaseSplitter/BaseSplitter.cpp
@@ -196,6 +196,8 @@ CBaseSplitterOutputPin::CBaseSplitterOutputPin(CAtlArray<CMediaType>& mts, LPCWS
 	, m_hrDeliver(S_OK) // just in case it were asked before the worker thread could be created and reset it
 	, m_fFlushing(false)
 	, m_eEndFlush(TRUE)
+	, m_rtPrev(0)
+	, m_rtOffset(0)
 	, m_MinQueuePackets((static_cast<CBaseSplitterFilter*>(m_pFilter))->GetMinQueuePackets())
 	, m_MaxQueuePackets((static_cast<CBaseSplitterFilter*>(m_pFilter))->GetMaxQueuePackets() * factor)
 	, m_MinQueueSize((static_cast<CBaseSplitterFilter*>(m_pFilter))->GetMinQueueSize())
