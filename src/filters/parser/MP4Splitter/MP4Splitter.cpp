6fdc6284d7e178fdfb005362ddde9557f01fde19
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-03-14 03:07:35 +0000

    Изменение : MP4Splitter - починил проигрывание некоторых звуковых файлов ... (я так не понял при чем здесь nBlockAlign и размер пакета).
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2275 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/parser/MP4Splitter/MP4Splitter.cpp
+++ src/filters/parser/MP4Splitter/MP4Splitter.cpp
@@ -1622,7 +1622,7 @@ bool CMP4SplitterFilter::DemuxLoop()
 
 					p->rtStop += (REFERENCE_TIME)(10000000.0 / track->GetMediaTimeScale() * sample.GetDuration());
 
-					if (pPairNext->m_value.index+1 >= track->GetSampleCount() || (int)p->GetCount() >= nBlockAlign) {
+					if (pPairNext->m_value.index + 1 >= track->GetSampleCount()) {
 						break;
 					}
 
