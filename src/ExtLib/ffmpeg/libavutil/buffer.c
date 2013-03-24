753f1e4b5a30bff9ab134bb3fd8f433163d4019c
szl <szl@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-03-24 15:59:51 +0000

    Обновление: ffmpeg git-cbf224b6.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2352 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/ExtLib/ffmpeg/libavutil/buffer.c
+++ src/ExtLib/ffmpeg/libavutil/buffer.c
@@ -276,6 +276,10 @@ static void pool_release_buffer(void *opaque, uint8_t *data)
 {
     BufferPoolEntry *buf = opaque;
     AVBufferPool *pool = buf->pool;
+
+    if(CONFIG_MEMORY_POISONING)
+        memset(buf->data, 0x2a, pool->size);
+
     add_to_pool(buf);
     if (!avpriv_atomic_int_add_and_fetch(&pool->refcount, -1))
         buffer_pool_free(pool);
