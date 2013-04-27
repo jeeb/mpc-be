0deffce7ca6f1d97bcefbeedf40defc6e0d31fc0
exodus8 <exodus8@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-04-27 23:17:45 +0000

    Изменение: дополнение к r.2570;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2571 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- build_clean_all.bat
+++ build_clean_all.bat
@@ -18,4 +18,4 @@ REM
 REM You should have received a copy of the GNU General Public License
 REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CALL build_2010.bat clean All
+CALL build.bat clean All
