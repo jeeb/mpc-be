e7e97b48e30055b664da4e1e67af9e6df3ef8557
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-05-04 01:20:32 +0000

    Изменение : прописал расширение .mpg для MpegSource.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2619 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/FGManager.cpp
+++ src/apps/mplayerc/FGManager.cpp
@@ -1744,6 +1744,7 @@ CFGManagerCustom::CFGManagerCustom(LPCTSTR pName, LPUNKNOWN pUnk, HWND hWnd, boo
 		pFGF->m_chkbytes.AddTail(_T("0,8,fffffc00ffe00000,4156000055000000"));
 		pFGF->m_chkbytes.AddTail(_T("0,8,,4D504C5330323030"));	// MPLS0200
 		pFGF->m_chkbytes.AddTail(_T("0,8,,4D504C5330313030"));	// MPLS0100
+		pFGF->m_extensions.AddTail(_T(".mpg"));
 		pFGF->m_extensions.AddTail(_T(".ts"));
 		pFGF->m_extensions.AddTail(_T(".m2ts"));
 		pFGF->m_extensions.AddTail(_T(".tp"));
