0e2efe562db4cb2351e3b7078bfb575d9c5e4138
szl <szl@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-05-04 21:51:54 +0000

    Изменение: дополнение к ревизии 2630, исправление построения ffmpeg.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2632 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/ExtLib/ffmpeg/libavutil/utils.c
+++ src/ExtLib/ffmpeg/libavutil/utils.c
@@ -94,8 +94,8 @@ unsigned av_int_list_length_for_size(unsigned elsize,
 
     if (!list)
         return 0;
+// ==> Start patch MPC
 #define LIST_LENGTH(type) \
-    // ==> Start patch MPC
     { type t = term, *l = (void*)list; for (i = 0; l[i] != t; i++); }
     // ==> End patch MPC
     switch (elsize) {
