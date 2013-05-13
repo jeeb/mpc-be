51c58985daf298d1d26f65f929f8d473c2666378
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-05-13 06:19:12 +0000

    Изменение : Windows 8, если приложение не закреплено на панели задач, то оно не получается сообщение "TaskbarButtonCreated", поэтому необходимо "принудительно" создавать ITaskbarList3 чтобы работали все "фишки" панели задач. Так же - обновил текст и перевод.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2692 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/MainFrm.cpp
+++ src/apps/mplayerc/MainFrm.cpp
@@ -914,6 +914,11 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 
 	s.SetMainWnd(this);
 
+	// Windows 8 - if app is not pinned on the taskbar, it's not receive "TaskbarButtonCreated" message. Bug ???
+	if (IsWinEight()) {
+		CreateThumbnailToolbar();
+	}
+
 	return 0;
 }
 
