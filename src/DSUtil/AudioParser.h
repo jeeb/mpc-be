f6c6377cff0406452d4367df58604c08ed7f26ef
v0lt <v0lt@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-05-23 18:31:55 +0000

    AudioParser: чистка от старого отключенного кода.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2764 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/DSUtil/AudioParser.h
+++ src/DSUtil/AudioParser.h
@@ -40,18 +40,7 @@
 #define EAC3_FRAME_TYPE_AC3_CONVERT  2
 #define EAC3_FRAME_TYPE_RESERVED     3
 
-//int GetAC3FrameSize  (const BYTE* buf); // AC3
-//int GetEAC3FrameSize (const BYTE* buf); // E-AC3
-//int GetMLPFrameSize  (const BYTE* buf); // TrueHD and MLP
-//int GetDTSFrameSize  (const BYTE* buf); // DTS
 int GetDTSHDFrameSize(const BYTE* buf); // DTS-HD, need >= 10 bytes
-//int GetADTSFrameSize (const BYTE* buf, int* headersize);
-
-//int ParseAC3Header     (const BYTE* buf, int* samplerate, int* channels, int* framelength, int* bitrate);
-//int ParseEAC3Header    (const BYTE* buf, int* samplerate, int* channels, int* framelength, int* frametype);
-//int ParseMLPHeader     (const BYTE* buf, int* samplerate, int* channels, int* framelength, WORD* bitdepth, bool* isTrueHD); // TrueHD and MLP
-//int ParseDTSHeader     (const BYTE* buf, int* samplerate, int* channels, int* framelength, int* tr_bitrate);
-//int ParseHdmvLPCMHeader(const BYTE* buf, int* samplerate, int* channels);
 bool ParseAACLatmHeader(const BYTE* buf, int len, int* samplerate, int* channels, BYTE* extra, unsigned int* extralen);
 
 DWORD GetDefChannelMask(WORD nChannels);
