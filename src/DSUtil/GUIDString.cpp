ab7ac1e7ca5bc4477748a24e36d0145fa27b6bbb
szl <szl@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-06-19 01:56:51 +0000

    Изменение: обновлени Id svn для файлов хранилища.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@2946 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/DSUtil/GUIDString.cpp
+++ src/DSUtil/GUIDString.cpp
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright (C) 2013 Alexandr Vodiannikov aka "Aleksoid1978" (Aleksoid1978@mail.ru)
+ * Copyright (C) 2013 Alexandr Vodiannikov aka "Aleksoid1978" (Aleksoid1978@mail.ru).
  *
  * This file is part of MPC-BE.
  *
