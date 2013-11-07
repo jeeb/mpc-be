bc854c81a4b042a0aae71c3171e041c9ef578c0b
szl <szl@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-11-07 08:33:51 +0000

    Обновление: VirtualDub 1.10.4.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@3736 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/ExtLib/VirtualDub/system/h/stdafx.h
+++ src/ExtLib/VirtualDub/system/h/stdafx.h
@@ -1,6 +1,12 @@
 /* MPC-BE comment out
+// Force C locale to avoid this warning:
+//
+// mmreg.h : warning C4819: The file contains a character that cannot be represented in the current code page (932). Save the file in Unicode format to prevent data loss
+#pragma setlocale("C")
+
 // Detect the Windows SDK in use and select Windows 2000 baseline
 // if the Vista SDK, else Windows 98 baseline.
+
 #ifdef _MSC_VER
 #include <ntverp.h>
 #else
