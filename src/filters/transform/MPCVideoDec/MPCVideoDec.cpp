639de274b4ccac36ab479981a58b6d96544d2207
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-11-14 13:04:55 +0000

    Изменение : MPCVideoDec - подкорректировал выбор форматов, теперь корректно выставляется NV12;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@3788 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/transform/MPCVideoDec/MPCVideoDec.cpp
+++ src/filters/transform/MPCVideoDec/MPCVideoDec.cpp
@@ -761,8 +761,10 @@ const AMOVIESETUP_MEDIATYPE sudPinTypesOut[] = {
 
 #define ___PIX_FMT_YUY2  AV_PIX_FMT_YUYV422
 #define ___PIX_FMT_YV12  AV_PIX_FMT_YUV420P
+#define ___PIX_FMT_NV12  ___PIX_FMT_YV12
 #define ___PIX_FMT_YV16  AV_PIX_FMT_YUV422P
 #define ___PIX_FMT_YV24  AV_PIX_FMT_YUV444P
+#define ___PIX_FMR_ARGB  AV_PIX_FMT_ARGB
 
 typedef struct {
 	const LPCTSTR          name;
@@ -773,9 +775,9 @@ typedef struct {
 
 static const SW_OUT_FMT s_sw_formats_def[] = {
 	{_T("YUY2"),  {&MEDIASUBTYPE_YUY2,  1, 16, '2YUY'}, FF_CSP_YUY2,                      ___PIX_FMT_YUY2 }, // PixFmt_YUY2
-	{_T("NV12"),  {&MEDIASUBTYPE_NV12,  2, 12, '21VN'}, FF_CSP_NV12,                      AV_PIX_FMT_NV12 }, // PixFmt_NV12
+	{_T("NV12"),  {&MEDIASUBTYPE_NV12,  2, 12, '21VN'}, FF_CSP_NV12,                      ___PIX_FMT_NV12 }, // PixFmt_NV12
 	{_T("YV12"),  {&MEDIASUBTYPE_YV12,  3, 12, '21VY'}, FF_CSP_420P|FF_CSP_FLAGS_YUV_ADJ, ___PIX_FMT_YV12 }, // PixFmt_YV12
-	{_T("RGB32"), {&MEDIASUBTYPE_RGB32, 1, 32, BI_RGB}, FF_CSP_RGB32,                     AV_PIX_FMT_ARGB }, // PixFmt_RGB32
+	{_T("RGB32"), {&MEDIASUBTYPE_RGB32, 1, 32, BI_RGB}, FF_CSP_RGB32,                     ___PIX_FMR_ARGB }, // PixFmt_RGB32
 };
 
 VIDEO_OUTPUT_FORMATS DXVAFormats[] = { // DXVA2
@@ -1799,7 +1801,7 @@ void CMPCVideoDecFilter::BuildOutputFormat()
 
 		// same format
 		MPCPixelFormat pf = GetOutPixFormat(m_pix_fmt);
-		if (pf != PixFmt_None && inqueue[pf]) {
+		if (pf != PixFmt_None) {
 			nSwIndex[nSwCount++] = pf;
 			inqueue[pf] = false;
 		}
@@ -2015,7 +2017,7 @@ HRESULT CMPCVideoDecFilter::CompleteConnect(PIN_DIRECTION direction, IPin* pRece
 				return hr;
 			}
 
-			BuildOutputFormat();
+			m_nSwRefresh = 2;
 		}
 
 		CLSID ClsidSourceFilter = GetCLSID(m_pInput->GetConnected());
@@ -2228,7 +2230,7 @@ MPCPixelFormat CMPCVideoDecFilter::GetOutPixFormat(GUID& subtype)
 MPCPixelFormat CMPCVideoDecFilter::GetOutPixFormat(AVPixelFormat av_pix_fmt)
 {
 	for (int i = 0; i < PixFmt_count; i++) {
-		if (s_sw_formats_def[i].av_pix_fmt == av_pix_fmt) {
+		if (s_sw_formats_def[i].av_pix_fmt == av_pix_fmt && m_fPixFmts[i]) {
 			return (MPCPixelFormat)i;
 		}
 	}
