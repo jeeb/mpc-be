00161e95b2fb3d24aba75cdcf8d5045e4d9cdb14
v0lt <v0lt@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-12-06 17:01:49 +0000

    Окончательная доработка 3955, 3970 (разобрался с битовыми операциями).
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@3972 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/transform/MPCVideoDec/FormatConverter.cpp
+++ src/filters/transform/MPCVideoDec/FormatConverter.cpp
@@ -111,7 +111,7 @@ MPCPixFmtType GetPixFmtType(AVPixelFormat av_pix_fmt)
 		return PFType_unspecified;
 	}
 
-	if ((pfdesc->flags ^ AV_PIX_FMT_FLAG_ALPHA) == AV_PIX_FMT_FLAG_PLANAR) {
+	if ((pfdesc->flags & ~AV_PIX_FMT_FLAG_ALPHA) == AV_PIX_FMT_FLAG_PLANAR) {
 		// must be planar type, ignore alpha channel, other flags are forbidden
 
 		if (pfdesc->log2_chroma_w == 1 && pfdesc->log2_chroma_h == 1) {
