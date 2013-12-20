c6765a38cc91565a3f68b05e104bc2d2586441e9
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2013-12-20 04:32:05 +0000

    Изменените : MPCVideoDec - не обрабатывались флаги интерлейс в режиме софтового декодирования, слишком рано очищался AVFrame - исправлено;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@4089 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/transform/MPCVideoDec/MPCVideoDec.cpp
+++ src/filters/transform/MPCVideoDec/MPCVideoDec.cpp
@@ -2525,7 +2525,6 @@ HRESULT CMPCVideoDecFilter::SoftwareDecode(IMediaSample* pIn, BYTE* pDataIn, int
 		else {
 			m_FormatConverter.Converting(pDataOut, m_pFrame);
 		}
-		av_frame_unref(m_pFrame);
 
 #if defined(_DEBUG) && 0
 		static REFERENCE_TIME	rtLast = 0;
@@ -2535,6 +2534,8 @@ HRESULT CMPCVideoDecFilter::SoftwareDecode(IMediaSample* pIn, BYTE* pDataIn, int
 #endif
 
 		SetTypeSpecificFlags(pOut);
+		av_frame_unref(m_pFrame);
+
 		hr = m_pOutput->Deliver(pOut);
 	}
 
