d4d89aceab67e51a165ff4eb2d4c492e5cc4e6ce
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2014-01-11 10:59:42 +0000

    Изменение : PGS(HDMV) субтитры - если размер субтитров больше чем видео, то пытаемся вписать в размер видео, особенно актуально для VSFilter, в случае упаковки PGS субтитров размером 1080p и видео меньшим размером. Так же и для встроенного ISR(встречается и такое видео). При вычислении нужной позиции - по горизонтали выравниваем по центру, по вертикали - отступ 10 пикселей от края.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@4201 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/Subtitles/HdmvSub.cpp
+++ src/Subtitles/HdmvSub.cpp
@@ -351,19 +351,23 @@ void CHdmvSub::Render(SubPicDesc& spd, REFERENCE_TIME rt, RECT& bbox)
 
 		if (pObject && rt >= pObject->m_rtStart && rt < pObject->m_rtStop) {
 
-			// To fit in current surface size on render - looks very crooked
-			/*
-			int delta_x = (m_VideoDescriptor.nVideoWidth - (pObject->m_horizontal_position + pObject->m_width)) * spd.w/m_VideoDescriptor.nVideoWidth;
-			int delta_y = (m_VideoDescriptor.nVideoHeight - (pObject->m_vertical_position + pObject->m_height)) * spd.h/m_VideoDescriptor.nVideoHeight;
+			{
+				// To fit in current surface size on render - looks very crooked, but better then nothing ...
+				int delta_y = (m_VideoDescriptor.nVideoHeight - (pObject->m_vertical_position + pObject->m_height)) * spd.h / m_VideoDescriptor.nVideoHeight;
+				if (spd.w < (pObject->m_horizontal_position + pObject->m_width)) {
+					pObject->m_horizontal_position = max(0, spd.w / 2 - pObject->m_width / 2);
+					if (spd.w < (pObject->m_horizontal_position + pObject->m_width)) {
+						pObject->m_horizontal_position = max(0, spd.w - pObject->m_width - 10);
+					}
+				}
 
-			if (spd.w < (pObject->m_horizontal_position + pObject->m_width)) {
-				pObject->m_horizontal_position = max(0, spd.w - pObject->m_width - delta_x);
+				if (spd.h < (pObject->m_vertical_position + pObject->m_height)) {
+					pObject->m_vertical_position = max(0, spd.h - pObject->m_height - delta_y);
+					if (spd.h < (pObject->m_vertical_position + pObject->m_height)) {
+						pObject->m_vertical_position = max(0, spd.h - pObject->m_height - 10);
+					}
+				}
 			}
-
-			if (spd.h < (pObject->m_vertical_position + pObject->m_height)) {
-				pObject->m_vertical_position = max(0, spd.h - pObject->m_height - delta_y);
-			}
-			*/
 
 			if (pObject->GetRLEDataSize() && pObject->m_width > 0 && pObject->m_height > 0 &&
 					spd.w >= (pObject->m_horizontal_position + pObject->m_width) &&
