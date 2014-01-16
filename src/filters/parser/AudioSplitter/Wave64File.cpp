68e4083071fa98b132543e11ae4a7f5119974ccd
v0lt <v0lt@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2014-01-16 17:27:39 +0000

    AudioSplitter: унификация кода Wave64-сплиттера.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@4266 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/parser/AudioSplitter/Wave64File.cpp
+++ src/filters/parser/AudioSplitter/Wave64File.cpp
@@ -22,6 +22,16 @@
 #include "../../../DSUtil/AudioParser.h"
 #include "Wave64File.h"
 
+#pragma pack(push, 1)
+typedef union {
+		struct {
+			BYTE guid[16];
+			LONGLONG size;
+		};
+		BYTE data[24];
+	} chunk64_t;
+#pragma pack(pop)
+
 //
 // Wave64File
 //
@@ -34,6 +44,7 @@ CWave64File::CWave64File()
 HRESULT CWave64File::Open(CBaseSplitterFile* pFile)
 {
 	m_pFile = pFile;
+	m_startpos = 0;
 
 	m_pFile->Seek(0);
 	BYTE data[40];
@@ -46,24 +57,17 @@ HRESULT CWave64File::Open(CBaseSplitterFile* pFile)
 	}
 	__int64 end = min((__int64)*(LONGLONG*)(data + 16), m_pFile->GetLength());
 
-#pragma pack(push, 8)
-	union {
-		struct {
-			BYTE guid[16];
-			LONGLONG size;
-		};
-		BYTE data[24];
-	} Chunk64;
-#pragma pack(pop)
+	chunk64_t Chunk64;
 
 	while (SUCCEEDED(m_pFile->ByteRead(Chunk64.data, sizeof(Chunk64))) && m_pFile->GetPos() < end) {
 		if (Chunk64.size < sizeof(Chunk64)) {
 			TRACE(L"CWave64File::Open() : bad chunk size\n");
 			return E_FAIL;
 		}
-		Chunk64.size -= sizeof(Chunk64);
 
 		__int64 pos = m_pFile->GetPos();
+		Chunk64.size -= sizeof(Chunk64);
+
 		if (memcmp(Chunk64.guid, w64_guid_fmt, 16) == 0) {
 			if (m_fmtdata || Chunk64.size < sizeof(PCMWAVEFORMAT) || Chunk64.size > 65536) {
 				TRACE(L"CWave64File::Open() : bad format\n");
@@ -76,9 +80,12 @@ HRESULT CWave64File::Open(CBaseSplitterFile* pFile)
 				TRACE(L"CWave64File::Open() : format can not be read.\n");
 				return E_FAIL;
 			}
-		} else if (memcmp(Chunk64.guid, w64_guid_data, 16) == 0) {
-			break;
-		} else if (memcmp(Chunk64.guid, w64_guid_list, 16) != 0
+		}
+		else if (memcmp(Chunk64.guid, w64_guid_data, 16) == 0) {
+			m_startpos	= m_pFile->GetPos();
+			m_length	= min(Chunk64.size, m_pFile->GetLength() - m_startpos);
+		}
+		else if (memcmp(Chunk64.guid, w64_guid_list, 16) != 0
 				&& memcmp(Chunk64.guid, w64_guid_fact, 16) != 0
 				&& memcmp(Chunk64.guid, w64_guid_levl, 16) != 0
 				&& memcmp(Chunk64.guid, w64_guid_junk, 16) != 0
@@ -92,12 +99,10 @@ HRESULT CWave64File::Open(CBaseSplitterFile* pFile)
 		m_pFile->Seek((pos + Chunk64.size + 7) & ~7i64);
 	}
 
-	if (memcmp(Chunk64.guid, w64_guid_data, 16) != 0 || !ProcessWAVEFORMATEX()) {
+	if (!m_startpos || !ProcessWAVEFORMATEX()) {
 		return E_FAIL;
 	}
 
-	m_startpos		= m_pFile->GetPos();
-	m_length		= min(Chunk64.size, m_pFile->GetLength() - m_startpos);
 	m_length		-= m_length % m_nBlockAlign;
 	m_endpos		= m_startpos + m_length;
 	m_rtduration	= 10000000i64 * m_length / m_nAvgBytesPerSec;
