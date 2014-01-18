0fd0f9a290100cd638c76a746cbcfc0ccfa06e4c
v0lt <v0lt@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2014-01-18 05:37:38 +0000

    Рендеринг субтитров: увеличено стандартное значение количества фрагментов в буфере до 10 (как в MPC-HC).
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@4284 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/AppSettings.cpp
+++ src/apps/mplayerc/AppSettings.cpp
@@ -1729,7 +1729,7 @@ void CAppSettings::UpdateRenderersData(bool fSave)
 
 		r.fResetDevice = !!pApp->GetProfileInt(IDS_R_SETTINGS, _T("ResetDevice"), TRUE);
 
-		r.nSPCSize = pApp->GetProfileInt(IDS_R_SETTINGS, IDS_RS_SPCSIZE, 4);
+		r.nSPCSize = pApp->GetProfileInt(IDS_R_SETTINGS, IDS_RS_SPCSIZE, 10);
 		r.nSPMaxTexRes = pApp->GetProfileInt(IDS_R_SETTINGS, IDS_RS_SPMAXTEXRES, 1280);
 		r.fSPCPow2Tex = !!pApp->GetProfileInt(IDS_R_SETTINGS, IDS_RS_POW2TEX, TRUE);
 		r.fSPCAllowAnimationWhenBuffering = !!pApp->GetProfileInt(IDS_R_SETTINGS, _T("SPCAllowAnimationWhenBuffering"), TRUE);
