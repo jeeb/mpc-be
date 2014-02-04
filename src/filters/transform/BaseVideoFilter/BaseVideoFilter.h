877e0deb496049fe4d2b685c2ab8b8932c349ccb
v0lt <v0lt@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2014-02-04 11:01:18 +0000

    Не переопределяем стандартные типы BYTE и WORD.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@4370 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/transform/BaseVideoFilter/BaseVideoFilter.h
+++ src/filters/transform/BaseVideoFilter/BaseVideoFilter.h
@@ -21,9 +21,6 @@
 
 #pragma once
 
-typedef unsigned char BYTE;
-typedef unsigned short WORD;
-
 bool BitBltFromP016ToP016(size_t w, size_t h, BYTE* dstY, BYTE* dstUV, int dstPitch, BYTE* srcY, BYTE* srcUV, int srcPitch);
 
 typedef struct {
