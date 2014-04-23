545c8036b9f81353f6595b7d0ec12d3da9bb0674
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2014-04-23 03:12:23 +0000

    Изменение : Из MPC-HC - Audio Switcher: Support fallback to another media type. Требуется для будущего изменения в аудио - декодере.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@4824 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/switcher/AudioSwitcher/StreamSwitcher.cpp
+++ src/filters/switcher/AudioSwitcher/StreamSwitcher.cpp
@@ -1052,7 +1052,20 @@ HRESULT CStreamSwitcherOutputPin::BreakConnect()
 HRESULT CStreamSwitcherOutputPin::CompleteConnect(IPin* pReceivePin)
 {
 	m_pPinConnection = CComQIPtr<IPinConnection>(pReceivePin);
-	return __super::CompleteConnect(pReceivePin);
+	HRESULT hr = __super::CompleteConnect(pReceivePin);
+
+	CStreamSwitcherInputPin* pIn = (static_cast<CStreamSwitcherFilter*>(m_pFilter))->GetInputPin();
+	CMediaType mt;
+	if (SUCCEEDED(hr) && pIn && pIn->IsConnected()
+			&& SUCCEEDED(pIn->GetConnected()->ConnectionMediaType(&mt)) && m_mt != mt) {
+		if (pIn->GetConnected()->QueryAccept(&m_mt) == S_OK) {
+			hr = m_pFilter->ReconnectPin(pIn->GetConnected(), &m_mt);
+		} else {
+			hr = VFW_E_TYPE_NOT_ACCEPTED;
+		}
+	}
+
+	return hr;
 }
 
 HRESULT CStreamSwitcherOutputPin::CheckMediaType(const CMediaType* pmt)
