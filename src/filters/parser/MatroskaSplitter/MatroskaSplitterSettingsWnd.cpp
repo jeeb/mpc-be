9c1906fabcbc79e20a072a3934e46c8e4db4a0d7
v0lt <v0lt@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2014-08-07 11:52:39 +0000

    MatroskaSplitter: исправлено отображение опции. Скорректирован размер окна.
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@5236 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/filters/parser/MatroskaSplitter/MatroskaSplitterSettingsWnd.cpp
+++ src/filters/parser/MatroskaSplitter/MatroskaSplitterSettingsWnd.cpp
@@ -55,10 +55,10 @@ bool CMatroskaSplitterSettingsWnd::OnActivate()
 	DWORD dwStyle = WS_VISIBLE | WS_CHILD | WS_TABSTOP;
 	CPoint p(10, 10);
 
-	m_cbLoadEmbeddedFonts.Create(ResStr(IDS_MKVSPLT_LOAD_EMBEDDED_FONTS), dwStyle | BS_AUTOCHECKBOX | BS_LEFTTEXT, CRect(p, CSize(IPP_SCALE(280), m_fontheight)), this, IDC_STATIC);
+	m_cbLoadEmbeddedFonts.Create(ResStr(IDS_MKVSPLT_LOAD_EMBEDDED_FONTS), dwStyle | BS_AUTOCHECKBOX | BS_LEFTTEXT, CRect(p, CSize(IPP_SCALE(290), m_fontheight)), this, IDC_STATIC);
 	p.y += IPP_SCALE(20);
 
-	m_cbCalcDuration.Create(ResStr(IDS_MKVSPLT_CALC_DURATION), dwStyle | BS_AUTOCHECKBOX | BS_LEFTTEXT, CRect(p, CSize(IPP_SCALE(280), m_fontheight)), this, IDC_STATIC);
+	m_cbCalcDuration.Create(ResStr(IDS_MKVSPLT_CALC_DURATION), dwStyle | BS_AUTOCHECKBOX | BS_LEFTTEXT, CRect(p, CSize(IPP_SCALE(290), m_fontheight)), this, IDC_STATIC);
 
 	if (m_pMSF) {
 		m_cbLoadEmbeddedFonts.SetCheck(m_pMSF->GetLoadEmbeddedFonts());
--- src/filters/parser/MatroskaSplitter/MatroskaSplitterSettingsWnd.h
+++ src/filters/parser/MatroskaSplitter/MatroskaSplitterSettingsWnd.h
@@ -42,7 +42,7 @@ public:
 	bool OnApply();
 
 	static LPCTSTR GetWindowTitle() { return MAKEINTRESOURCE(IDS_FILTER_SETTINGS_CAPTION); }
-	static CSize GetWindowSize() { return CSize(350, 250); }
+	static CSize GetWindowSize() { return CSize(310, 53); }
 
 	DECLARE_MESSAGE_MAP()
 };
