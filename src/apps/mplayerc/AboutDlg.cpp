928e27bad9bd0a67c354614f68a57f392a478458
aleksoid <aleksoid@d191f3aa-8897-45be-b9a4-56e5fb472b67>
2014-08-07 01:59:05 +0000

    Изменение : "О программе" - добавлена поддержка Visual Studio 2013 update 3;
    git-svn-id: svn://mpc-next.ru/mpc-be/trunk@5227 d191f3aa-8897-45be-b9a4-56e5fb472b67

--- src/apps/mplayerc/AboutDlg.cpp
+++ src/apps/mplayerc/AboutDlg.cpp
@@ -60,7 +60,9 @@ BOOL CAboutDlg::OnInitDialog()
 #endif
 #elif defined(_MSC_VER)
 #if (_MSC_VER == 1800)		// 2013
-#if (_MSC_FULL_VER == 180030501)
+#if (_MSC_FULL_VER == 180030723)
+	m_MPCCompiler = _T("MSVC 2013.3");
+#elif (_MSC_FULL_VER == 180030501)
 	m_MPCCompiler = _T("MSVC 2013.2");
 #else
 	m_MPCCompiler = _T("MSVC 2013");
